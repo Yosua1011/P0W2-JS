03338ad13b38a5a7da018ffc5180239df2b12a87
Yosua Tresno Surojo <yosua1011@gmail.com>
2017-07-17 02:20:25 +0700

    Generated forlooping.js to print 20 increment and 20 decrement line

---
+++ Monday/forlooping.js
@@ -0,0 +1,7 @@
+for(var deret = 1; deret <= 20; deret += 1) {
+  console.log(deret + ' - I love coding ');
+}
+
+for(var deret = 20; deret > 0; deret -= 1) {
+  console.log(deret + ' - I will become fullstack developer');
+}
