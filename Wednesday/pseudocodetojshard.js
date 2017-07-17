7900e9e73d8c5c5234dbb4b07329ddba9eac62df
Yosua Surojo <Yosua Surojo>
2017-07-17 20:10:04 +0700

    whiletask.js & pseudocodetojshard.js generated

---
+++ Wednesday/pseudocodetojshard.js
@@ -0,0 +1,32 @@
+/* Tugas 1 */
+var total = 2;
+for (var index = 1; index <= 100; index ++) {
+	if (index % 2 != 0) {
+		total =  total + index;
+	}
+	else if (index % 2 == 0) {
+		total = total - index;
+	}
+}
+
+console.log('Total: ' + total);
+
+/* Tugas 2 */
+var pagar = '';
+for (var index1 = 1; index1 <= 10; index1++) {
+	for (var index2 = 1; index2 <= 10; index2++) {
+		pagar += '#';
+	}
+	console.log(pagar);
+    var pagar = '';
+}
+
+/* Tugas 3 */
+var bintang = '';
+for (var index1 = 1; index1 <= 10; index1++) {
+	for (var index2 = 0; index2 <= index1; index2++) {
+		bintang += '*';
+	}
+	console.log(bintang);
+    var bintang = '';
+}
---
+++ Wednesday/whiletask.js
@@ -0,0 +1,13 @@
+var angka = 0;
+while(angka <= 20) {
+	console.log(angka + ' - I love coding');
+	angka += 2;
+}
+
+console.log('-------------------');
+
+var number = 20;
+while(number >= 2) {
+  console.log(number + ' - I will become fullstack developer');
+  number -= 2;
+}
